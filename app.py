18296bf5493efa6b6da0cd4aba5b3df79dc03975
timcrmdev <106087554+timcrmdev@users.noreply.github.com>
2023-08-14 00:33:10 +0100

    created environment variables for db credentials

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 # import psycopg2 module to connect to PostgreSQL database
 import psycopg2
 
+# import 'os' to access environment variables.
+import os
+
+# Retrieve environment variables using os.environ.get('VARIABLE_NAME')
+db_host = os.environ.get('DB_HOST')
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_port = os.environ.get('DB_PORT')
+
+
+
 # Define database info
-hostname = 'localhost'
-database = 'valuation_tool_db'
-username = 'postgres'
-pwd = 'Dice123!'
-port_id = '5432'
+hostname = db_host
+database = db_name
+username = db_user
+pwd = db_password
+port_id = db_port
 
 # Reset connection and cursor
 conn = None
@@ -28,7 +40,7 @@ try:
 
     # Insert data into table
     insert_script = 'INSERT INTO car_make_tbl (make_id, make) VALUES(%s, %s)'
-    insert_values = ('2', 'audi')
+    insert_values = ('4', 'volkswagen')
 
     # Execute insert script to insert values into the table
     cursor.execute(insert_script, insert_values)
