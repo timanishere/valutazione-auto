31f77053e5211cc9a9154e5dab23c65f07fbd941
timcrmdev <106087554+timcrmdev@users.noreply.github.com>
2023-08-16 21:32:30 +0100

    Extract car data from all colours and models

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWeb
 
 # Check connection
 try:
-    
     # Retrieve environment variables using os.environ.get('VARIABLE_NAME')
     db_host = os.environ.get('DB_HOST')
     db_name = os.environ.get('DB_NAME')
@@ -35,198 +34,234 @@ try:
         user = db_user,
         password = db_password,
         port = db_port
-    )
+    )    
+except Exception as error:
+        print(error)
+        print('Could not connect to db')
 
-    # Open a cursor to perform SQL operationan and return data as a dictionary
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+# Open a cursor to perform SQL operationan and return data as a dictionary
+cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    # Query database to get list of colours
-    cursor.execute('SELECT colour, autoscout_url_colour FROM car_colour_tbl')
+# Query database to get list car make and model
+cursor.execute('SELECT make,model,autoscout_url_make, autoscout_url_model FROM car_make_and_model_tbl')
 
-    # Fetch data from database
-    colour_list_data_arr_raw = cursor.fetchall()
+# Fetch data from database
+make_and_model_list_data_arr_raw = cursor.fetchall()
 
-    # Set colour of car. Use index
-    i = 2
+number_of_make_and_model = len(make_and_model_list_data_arr_raw)
 
-    # Get colour name and key
-    car_colour = colour_list_data_arr_raw[i][0]
-    car_colour_key = colour_list_data_arr_raw[i][1]
-
-    print('car_color: ' + car_colour)
-
-    # Query database to get list car make and model
-    cursor.execute('SELECT make,model,autoscout_url_make, autoscout_url_model FROM car_make_and_model_tbl')
-
-    # Fetch data from database
-    make_and_model_list_data_arr_raw = cursor.fetchall()
-
-    # Set the make and model. Use index
-    i = 0
+# Loop through every make and model
+for i in range(0, number_of_make_and_model):
 
     # Get make and model
     car_make = make_and_model_list_data_arr_raw[i][0]
     car_model = make_and_model_list_data_arr_raw[i][1]
     car_make_key = make_and_model_list_data_arr_raw[i][2]
     car_model_key = make_and_model_list_data_arr_raw[i][3]
-    
-    print('car_make: ' + car_make)
-    print('car_model: ' + car_model)
 
-except Exception as error:
-        print(error)
-        print('Could not connect to db')
+    # Query database to get list of colours
+    cursor.execute('SELECT * FROM car_colour_tbl ORDER BY colour_id')
 
-# The main page to scrape data from
-main_site = 'https://www.autoscout24.it/lst/' + car_make_key + '/' + car_model_key + '/' + car_colour_key + '?atype=C&cy=I&damaged_listing=exclude&desc=0&powertype=kw&search_id=1s8324o1du&sort=standard&source=homepage_search-mask&ustate=N%2CU'
+    # Fetch data from database
+    colour_list_data_arr_raw = cursor.fetchall()
 
-# Print messsage in terminal to know that scraping is in progress
-print(f'Scraping {main_site} in progress...')
+    number_of_colours = len(colour_list_data_arr_raw)
 
-# Get information from the webpage
-webpage = requests.get(main_site, headers=HEADERS,).text
+    # Loop through every colour
+    for i in range(0, number_of_colours):
 
-# Create instance of beautifulsoup for the webpage
-soup = BeautifulSoup(webpage, 'lxml')
+        # Get colour name and key
+        car_colour = colour_list_data_arr_raw[i][1]
+        car_colour_key = colour_list_data_arr_raw[i][2]
 
-# Get advert listing elements
-advert_containers_arr = soup.findAll('article', class_='cldt-summary-full-item listing-impressions-tracking list-page-item false ListItem_article__ppamD')
+        # The main page to scrape data from
+        main_site = 'https://www.autoscout24.it/lst/' + car_make_key + '/' + car_model_key + '/' + car_colour_key + '?atype=C&cy=I&damaged_listing=exclude&desc=0&powertype=kw&search_id=1s8324o1du&sort=standard&source=homepage_search-mask&ustate=N%2CU&page=1'
+
+        # Print messsage in terminal to know that scraping is in progress
+        print(f'Scraping {main_site} in progress...')
+
+        # Get information from the webpage
+        webpage = requests.get(main_site, headers=HEADERS,).text
+
+        # Create instance of beautifulsoup for the webpage
+        soup = BeautifulSoup(webpage, 'lxml')
+
+        # Get paginator
+        number_of_pages_element_arr = soup.findAll('button', class_='FilteredListPagination_button__41hHM')
+
+        # Get the last number of the page
+        last_page_raw = number_of_pages_element_arr[2]
+
+        last_page_raw = str(last_page_raw)
+
+        # Clean string to get number
+        try:
+            last_page = last_page_raw.split('>')
+            last_page = last_page[1]
+            last_page = last_page.replace('</button', '')
+            last_page = int(last_page)
+        except:
+            last_page = 2
+
+        counter = 0 
+
+        # Loop through every page
+        for i in range(1, last_page):
+
+            print(f'scraping page {i}')
+            i = str(i)
+
+            # The main page to scrape data from
+            main_site_loop = 'https://www.autoscout24.it/lst/' + car_make_key + '/' + car_model_key + '/' + car_colour_key + '?atype=C&cy=I&damaged_listing=exclude&desc=0&powertype=kw&search_id=1s8324o1du&sort=standard&source=homepage_search-mask&ustate=N%2CU&page=' + i
+
+            # Get information from mainsite
+            webpage_loop = requests.get(main_site_loop, headers=HEADERS,).text
+
+            # Create instance of beautifulsoup for webpage_loop
+            soup = BeautifulSoup(webpage_loop, 'lxml')
+            
+            # Get advert listing elements
+            advert_containers_arr = soup.findAll('article', class_='cldt-summary-full-item listing-impressions-tracking list-page-item false ListItem_article__ppamD')
+
+            # Loop through array of adverts
+            for advert_container in advert_containers_arr:
+
+                # Get vehicle details container
+                vehicle_details_container = advert_container.find('div', class_='VehicleDetailTable_container__mUUbY')
+
+                # Get vehicle details items
+                vehicle_details_item_raw = vehicle_details_container.findAll('span', class_='VehicleDetailTable_item__koEV4')
+
+                try: 
+                    # Get price
+                    vehicle_price = advert_container.find('p', class_='Price_price__WZayw PriceAndSeals_current_price__XscDn').text
+                except:
+                    vehicle_price = advert_container.find('span', class_='SuperDeal_highlightContainer__EPrZr').text
+                    vehicle_price = vehicle_price.split(',')
+                    vehicle_price = vehicle_price[0]
+
+                vehicle_price = vehicle_price.replace(',-', '')
+                vehicle_price = vehicle_price.split(' ')
+                vehicle_price = vehicle_price[1]
+                vehicle_price = vehicle_price.replace('.', '')
+
+                # Convert item into array
+                km_raw = vehicle_details_item_raw[0]
+
+                # Convert object into a string
+                km_raw = str(km_raw)
+
+                # Convert item to an array
+                km_raw = km_raw.split('></use></svg>')
+
+                # Remove the characters from the string
+                km = km_raw[1].replace('km</span>', '')
+                km = km.replace('.', '')
+                km = km.replace(' ', '')
+
+                # Convert item into array
+                gear_raw = vehicle_details_item_raw[1]
+
+                # Convert object into a string
+                gear_raw = str(gear_raw)
+
+                # Convert item to an array
+                gear_raw = gear_raw.split('></use></svg>')
+
+                # Remove <span. to get string
+                gear = gear_raw[1].replace('</span>', '')
+
+                # Convert item into array
+                year_raw = vehicle_details_item_raw[2]
+
+                # Convert object into a string
+                year_raw = str(year_raw)
+
+                # Convert item to an array
+                year_raw = year_raw.split('></use></svg>')
+
+                # Remove <span> to get string
+                year_raw = year_raw[1].replace('</span>', '').split('/')
+
+                # Select the year
+                try:
+                    year = year_raw[1]
+                except: 
+                    year = None
+
+                # Convert item into array
+                fuel_type_raw = vehicle_details_item_raw[3]
+
+                # Convert object into a string
+                fuel_type_raw = str(fuel_type_raw)
+
+                # Convert item to an array
+                fuel_type_raw = fuel_type_raw.split('></use></svg>')
+
+                # Remove <span> to get string
+                fuel_type = fuel_type_raw[1].replace('</span>', '')
+
+                # Convert item into array
+                kw_raw = vehicle_details_item_raw[4]
+
+                # Convert object into a string
+                kw_raw = str(kw_raw)
+
+                # Convert item to an array
+                kw_raw = kw_raw.split('></use></svg>')
+
+                # Remove <span> to get string
+                kw_raw = kw_raw[1].replace('</span>', '').split(' ')
+
+                kw = kw_raw[0]
+
+                print(f'{car_make}|{car_model}|{car_colour}|{year}|{gear}|{fuel_type}|{kw}|{km}|{vehicle_price}')
+                # EXTRACT DATA: ENDS
 
 
-# Create a loop to print out the details of all adverts
-for advert_container in advert_containers_arr:
+            # Retrieve environment variables using os.environ.get('VARIABLE_NAME')
+            # db_host = os.environ.get('DB_HOST')
+            # db_name = os.environ.get('DB_NAME')
+            # db_user = os.environ.get('DB_USER')
+            # db_password = os.environ.get('DB_PASSWORD')
+            # db_port = os.environ.get('DB_PORT')
 
-    # Get vehicle details container
-    vehicle_details_container = advert_container.find('div', class_='VehicleDetailTable_container__mUUbY')
+            # Define database info
+            # hostname = db_host
+            # database = db_name
+            # username = db_user
+            # pwd = db_password
+            # port_id = db_port
 
-    # Get vehicle details items
-    vehicle_details_item_raw = vehicle_details_container.findAll('span', class_='VehicleDetailTable_item__koEV4')
+            # # Reset connection and cursor
+            # conn = None
+            # cursor = None
 
-    try: 
-        # Get price
-        vehicle_price = advert_container.find('p', class_='Price_price__WZayw PriceAndSeals_current_price__XscDn').text
-    except:
-        vehicle_price = advert_container.find('span', class_='SuperDeal_highlightContainer__EPrZr').text
+            # # Check connection
+            # try:
+            #     # Connect to database
+            #     conn = psycopg2.connect(
+            #         host = hostname,
+            #         dbname = database,
+            #         user = username,
+            #         password = pwd,
+            #         port = port_id
+            #     )
 
-    vehicle_price = vehicle_price.replace(',-', '')
-    vehicle_price = vehicle_price.split(' ')
-    vehicle_price = vehicle_price[1]
-    vehicle_price = vehicle_price.replace('.', '')
+            #     # Open a cursor to perform SQL operationa
+            #     cursor = conn.cursor()
 
-    # Convert item into array
-    km_raw = vehicle_details_item_raw[0]
+            #     # Insert data into table
+            #     insert_script = 'INSERT INTO car_prices_tbl (price_id, make_id, model_id, year_id, colour_id, gear_id, fuel_type_id, km, km_range, kw, price) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            #     insert_values = ('3', '1', '1', '16', '3', '1', '1', '20453', '20000-30000', '61', '100')
 
-    # Convert object into a string
-    km_raw = str(km_raw)
+            #     # Execute insert script to insert values into the table
+            #     cursor.execute(insert_script, insert_values)
 
-    # Convert item to an array
-    km_raw = km_raw.split('></use></svg>')
+            #     # Commit execution
+            #     conn.commit()
+            #     print('Added to db')
 
-    # Remove the characters from the string
-    km = km_raw[1].replace('km</span>', '')
-    km = km.replace('.', '')
-    km = km.replace(' ', '')
-
-    # Convert item into array
-    gear_raw = vehicle_details_item_raw[1]
-
-    # Convert object into a string
-    gear_raw = str(gear_raw)
-
-    # Convert item to an array
-    gear_raw = gear_raw.split('></use></svg>')
-
-    # Remove <span. to get string
-    gear = gear_raw[1].replace('</span>', '')
-
-    # Convert item into array
-    year_raw = vehicle_details_item_raw[2]
-
-    # Convert object into a string
-    year_raw = str(year_raw)
-
-    # Convert item to an array
-    year_raw = year_raw.split('></use></svg>')
-
-    # Remove <span> to get string
-    year_raw = year_raw[1].replace('</span>', '').split('/')
-
-    # Select the year
-    year = year_raw[1]
-
-    # Convert item into array
-    fuel_type_raw = vehicle_details_item_raw[3]
-
-    # Convert object into a string
-    fuel_type_raw = str(fuel_type_raw)
-
-    # Convert item to an array
-    fuel_type_raw = fuel_type_raw.split('></use></svg>')
-
-    # Remove <span> to get string
-    fuel_type = fuel_type_raw[1].replace('</span>', '')
-
-    # Convert item into array
-    kw_raw = vehicle_details_item_raw[4]
-
-    # Convert object into a string
-    kw_raw = str(kw_raw)
-
-    # Convert item to an array
-    kw_raw = kw_raw.split('></use></svg>')
-
-    # Remove <span> to get string
-    kw_raw = kw_raw[1].replace('</span>', '').split(' ')
-
-    kw = kw_raw[0]
-
-    print(f'{car_make}|{car_model}|{car_colour}|{year}|{gear}|{fuel_type}|{kw}|{km}|{vehicle_price}')
-    # EXTRACT DATA: ENDS
-
-
-# Retrieve environment variables using os.environ.get('VARIABLE_NAME')
-# db_host = os.environ.get('DB_HOST')
-# db_name = os.environ.get('DB_NAME')
-# db_user = os.environ.get('DB_USER')
-# db_password = os.environ.get('DB_PASSWORD')
-# db_port = os.environ.get('DB_PORT')
-
-# Define database info
-# hostname = db_host
-# database = db_name
-# username = db_user
-# pwd = db_password
-# port_id = db_port
-
-# # Reset connection and cursor
-# conn = None
-# cursor = None
-
-# # Check connection
-# try:
-#     # Connect to database
-#     conn = psycopg2.connect(
-#         host = hostname,
-#         dbname = database,
-#         user = username,
-#         password = pwd,
-#         port = port_id
-#     )
-
-#     # Open a cursor to perform SQL operationa
-#     cursor = conn.cursor()
-
-#     # Insert data into table
-#     insert_script = 'INSERT INTO car_prices_tbl (price_id, make_id, model_id, year_id, colour_id, gear_id, fuel_type_id, km, km_range, kw, price) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-#     insert_values = ('3', '1', '1', '16', '3', '1', '1', '20453', '20000-30000', '61', '100')
-
-#     # Execute insert script to insert values into the table
-#     cursor.execute(insert_script, insert_values)
-
-#     # Commit execution
-#     conn.commit()
-#     print('Added to db')
-
-# except Exception as error:
-#         print(error)
-#         print('Could not connect to db')
+            # except Exception as error:
+            #         print(error)
+            #         print('Could not connect to db')
